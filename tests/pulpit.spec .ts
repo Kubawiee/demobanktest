ad0e34a2434b04ec4364c67207fdfe7a06f9f4e3
kubaw <kubaw@LAPTOP-7A24ETEU>
2024-04-29 18:44:45 +0200

    first aaa impemetation

--- tests/pulpit.spec .ts
+++ tests/pulpit.spec .ts
@@ -4,12 +4,21 @@ import { test, expect } from '@playwright/test';
 test.describe('User login to Ecomerce', () => {
 
   test('login with correct credentails', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('kuba1234');
-    await page.getByTestId('password-input').fill('password');
+    //Arrange 
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'kuba1234';
+    const userPassword = 'password';
+    const expectedUserName = 'Jan Demobankowy';
+
+    //Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
-    
-    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+
+    //Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+
   });
 
   test('login with wrong to short login', async ({ page }) => {
